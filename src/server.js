f93b4c369d7547b3a65e3148fa6510f49e3ccd2a
Pratan naimjoi <52064201+pratannaimjoi@users.noreply.github.com>
2020-02-29 16:47:30 +0700

    Update and rename src/server.js to  src/server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,17 @@ var express = require ("express");
 var bodyParser = ต้องการ ("body-parser");
 var mongodb = ต้องการ ("mongodb");
 var ObjectID = mongodb.ObjectID;
+http.createServer(function (request, response) {  
+   // Send the HTTP header   
+   // HTTP Status: 200 : OK  
+   // Content Type: text/plain  
+   response.writeHead(200, {'Content-Type': 'text/plain'});  
+   // Send the response body as "Hello World"  
+   response.end('Hello World\n');  
+}).listen(8081);  
+// Console will print the message  
+console.log('Server running at http://127.0.0.1:8081/');  
+ 
 
 var CONTACTS_COLLECTION = "ผู้ติดต่อ";
 
